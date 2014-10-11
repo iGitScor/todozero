1508a8de542991463611bb614214a6efef43a165
monsieurluge <monsieurluge@gmail.com>
2014-10-11 21:41:37 +0000

    First commit

---
+++ js/todo.js
@@ -0,0 +1,19 @@
+function Todo() {
+  this.init();
+}
+
+Todo.prototype.clear = function() {
+
+};
+
+Todo.prototype.create = function() {
+
+};
+
+Todo.prototype.delete = function() {
+
+};
+
+Todo.prototype.init = function() {
+
+};
---
+++ js/todolist.js
@@ -0,0 +1,25 @@
+function TodoList() {
+  var todos = new Array();
+
+  this.init();
+}
+
+TodoList.prototype.addTodo = function() {
+
+};
+
+TodoList.prototype.clear = function() {
+
+};
+
+TodoList.prototype.editTodo = function() {
+
+};
+
+TodoList.prototype.init = function() {
+
+};
+
+TodoList.prototype.removeTodo = function() {
+
+};
