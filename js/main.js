f5748fec6990e1be2ed4944e6cf6a34febd1203d
MonsieurLuge <monsieurluge@gmail.com>
2014-10-15 23:14:36 +0200

    Start to work on the tabs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,62 @@
-/*var element = document.querySelector("#greeting");
-element.innerText = "Hello, world!";
-*/
+$(document).ready(function() {
+    $('.tab-link').each(function() {
+        initTabLink($(this));
+    });
+
+    closeAllOtherTabs('now');
+});
+
+/**
+ * [openTab description]
+ * @param  {[type]} tabName [description]
+ * @return {[type]}         [description]
+ */
+function openTab(tabName) {
+    var tab = $('#' + tabName);
+
+    if (tab.length === 0) {
+        return false;
+    }
+
+    closeAllOtherTabs(tabName);
+
+    tab.show();
+};
+
+/**
+ * [closeAllOtherTabs description]
+ * @param  {[type]} tabName [description]
+ * @return {[type]}         [description]
+ */
+function closeAllOtherTabs(tabName) {
+    var tab = $('#' + tabName);
+
+    if (tab.length === 0) {
+        return false;
+    }
+
+    $('.tab').each(function() {
+        if ($(this).attr('id') !== tab.attr('id')) {
+            $(this).hide();
+        }
+    });
+};
+
+/**
+ * [initTabLink description]
+ * @param  {[type]} tabLink [description]
+ * @return {[type]}         [description]
+ */
+function initTabLink(tabLink) {
+    tabLink.click(function(event) {
+        event.preventDefault();
+
+        $('.tab-link').each(function() {
+            $(this).removeClass('active');
+        });
+
+        tabLink.addClass('active');
+
+        openTab(tabLink.attr('data-tab'));
+    });
+}
